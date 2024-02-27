03e3f48e9cf596906bf7a7d3d05ee35d5c95752f
Guillermo Galindo <guillegalindo45@gmail.com>
2024-02-27 10:22:42 -0600

    Correcion del schema para darle mejores validaciones a la base de datos

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,30 @@
 const  {Schema, model} =  require('mongoose');
 
 const userSchema = new Schema({
-    name: String,
-    lastname: String,
-    email: String,
-    password: String,
+    name: {
+        type: String,
+        required: true,
+        minlength: [2, 'El nombre debe tener al menos 2 caracteres'],
+        maxlength: [50, 'El nombre no puede tener más de 50 caracteres']
+    },
+    lastname: {
+        type: String,
+        required: true,
+        minlength: [2, 'El apellido debe tener al menos 2 caracteres'],
+        maxlength: [50, 'El apellido no puede tener más de 50 caracteres']
+    },    
+    email: {
+        type: String,
+        required: true,
+        unique: true, // Asegurar unico el correo
+        //match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Por favor, ingrese un correo electrónico válido']
+    },    
+    password: {
+        type: String,
+        required: true,
+        minlength: [6, 'La contraseña debe tener al menos 6 caracteres'],
+        maxlength: [16, 'La contraseña no puede tener más de 16 caracteres']
+    },    
 },{
     timestamps: true
 
